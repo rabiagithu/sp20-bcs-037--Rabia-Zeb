024741f88041ce4b4f94abea4c8ed22495f2b214
rabiagithu <102785200+rabiagithu@users.noreply.github.com>
2022-09-26 20:58:06 +0500

    Create task2.js
    lab task2

---
+++ LAB ASSIGNMENT OR TASKS/task2.js
@@ -0,0 +1,47 @@
+/* function sandwich_calculator(bread){  
+       return (Math.floor(bread/2)+" Sandwiches can be made!");  
+       }  
+       console.log(sandwich_calculator(7)); */ 
+ function sandwich_calculator(bread, cheese) { 
+        var sandwiches = 0; 
+        while (true) { 
+            if (bread >= 2) { 
+                bread -= 2; 
+                if (cheese >= 1) { 
+                    cheese -= 1 
+                    sandwiches += 1; 
+                } 
+                else { 
+                    console.log(sandwiches + " Sandwiches can be made!"); 
+                    break; 
+                } 
+            } 
+            else { 
+                console.log(sandwiches + " Sandwiches can be made!"); 
+                break; 
+            } 
+        } 
+    } 
+    sandwich_calculator(26, 12);
+
+
+    var student = { 
+            name: "David Rayy", 
+            sclass: "X", 
+            rollno: 12 
+        }; 
+        console.log("Name: ", student.name, "Roll No.: ", student.rollno, "Class: ", student.sclass);
+
+
+
+
+
+
+        var student = { 
+                name: "David Rayy", 
+                sclass: "X", 
+                rollno: 12 
+            }; 
+            console.log(student); 
+            delete student.rollno; 
+            console.log(student);
